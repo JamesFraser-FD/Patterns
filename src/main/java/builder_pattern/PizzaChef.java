2696a9e812eccb11ff6498553402cc58ec2c07a3
James Fraser <james.fraser@fanduel.com>
2021-09-09 09:39:03 +0100

    Reformat

--- src/main/java/builder_pattern/PizzaChef.java
+++ src/main/java/builder_pattern/PizzaChef.java
@@ -3,10 +3,18 @@ package builder_pattern;
 public class PizzaChef {
 
     public Pizza getMargharita() {
-        return new Pizza.Builder().withTomatoSauce().withCheese().cook();
+        return new Pizza.Builder()
+                .withTomatoSauce()
+                .withCheese()
+                .cook();
     }
 
     public Pizza getAmericanHot() {
-        return new Pizza.Builder().withTomatoSauce().withCheese().withPepperoni().withJalapenos().cook();
+        return new Pizza.Builder()
+                .withTomatoSauce()
+                .withCheese()
+                .withPepperoni()
+                .withJalapenos()
+                .cook();
     }
 }
